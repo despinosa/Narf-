7c40761abc14d4e2f5a767b9eeae4de254702053
InesAAR <ines.aar93@gmail.com>
2016-10-20 21:57:50 -0500

    Delete se muere

--- Narf! Tests/CaseTests.cs
+++ Narf! Tests/CaseTests.cs
@@ -17,36 +17,43 @@ namespace Narf.Model.Test {
         Substance = "SNDRA", Dose = 12M, Subject = "Pinky", Weight = 246.9M,
         Maze = Maze.Plus, Preview = new byte[10], VideoHash = rnd.Next()
       };
-      var case2_ = new Case() {
-        Date = new DateTime(2016, 7, 18), Duration = (short)new TimeSpan(0, 4, 53).TotalSeconds,
-        Substance = "SRA", Dose = 10M, Subject = "Rat", Weight = 246.1M,
-        Maze = Maze.Plus, Preview = new byte[10], VideoHash = rnd.Next()
-      };
-
+      
       Case[] before;
       using (var context = new Entities()) {
         before = context.Cases.ToArray();
         context.Cases.Add(case1_);
         context.SaveChanges();
-        context.Cases.Add(case2_);
-        context.SaveChanges();
+        
       }
       using (var context = new Entities()) {
         var after = context.Cases.ToArray();
         Case new_ = after.First( c => ! before.Contains(c) );
         case1_.Id = new_.Id;
         Assert.AreEqual(new_.Id, case1_.Id);
+        Assert.AreEqual(new_.Date, case1_.Date);
+        Assert.AreEqual(new_.Duration, case1_.Duration);
+        Assert.AreEqual(new_.Substance, case1_.Substance);
+        Assert.AreEqual(new_.Dose, case1_.Dose);
+        Assert.AreEqual(new_.Subject, case1_.Subject);
+        Assert.AreEqual(new_.Weight, case1_.Weight);
+        Assert.AreEqual(new_.Maze, case1_.Maze);
       }
     }
     [TestMethod()]
     public void Delete() {
-      Case target;
+      Random rnd = new Random();
+      var case2_ = new Case() {
+        Id = 2, Date = new DateTime(2016, 7, 18), Duration = (short)new TimeSpan(0, 4, 53).TotalSeconds,
+        Substance = "SRA", Dose = 10M, Subject = "Rat", Weight = 246.1M,
+        Maze = Maze.Plus, Preview = new byte[10], VideoHash = rnd.Next()
+      };
+      
       using (var context = new Entities()) {
-        target = context.Cases.Find(4);
+        context.Cases.Add(case2_);
+        context.SaveChanges();
+        Case target = context.Cases.Find(2);
         context.Cases.Remove(target);
         context.SaveChanges();
-      }
-      using (var context = new Entities()) {
         Assert.IsFalse(context.Cases.Contains<Case>(target));
       }
     }
