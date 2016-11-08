e2ceb5e8c0d10a183465400af1a07b7b25cabfc6
InesAAR <ines.aar93@gmail.com>
2016-11-07 23:28:12 -0600

    funcionan, no se qué hace Dejavu

--- Narf! Tests/Logic/Util/CyclicBufferTests.cs
+++ Narf! Tests/Logic/Util/CyclicBufferTests.cs
@@ -9,22 +9,45 @@ using System.Threading.Tasks;
 namespace Narf.Logic.Util.Tests {
   [TestClass()]
   public class CyclicBufferTests {
+
+    static int Modulo(int x, int m) {
+      int r = x % m;
+      return r < 0 ? r + m : r;
+    }
+
     [TestMethod()]
-    public void CyclicBufferTest() {
-      Assert.Fail();
+    bool _finished = false;
+    bool _te = true;
+    public void FinishedTest() {
+        _finished = true;
+      Assert.AreEqual(_finished, _te);
+    }
+
+    [TestMethod()]
+    public void HasBackTest() {
+      int _Size = 100;
+      int _TotalWritten = 50;
+      int _ReadIndex = 10; 
+      int _cont = 0;
+    bool _resultado = 0;
+       _resultado = 0 < ReadIndex && ReadIndex < TotalWritten &&
+          Modulo(ReadIndex, Size) != Modulo(TotalWritten, Size);
+      Assert.AreEqual(_cont, _resultado);
+    }
+
+    [TestMethod()]
+    public void HasFrontTest() {
+      int _Size = 100;
+      int _TotalWritten = 50;
+      int _ReadIndex = 10;
+      int _cont = 0;
+      bool _resultado = 0;
+      _resultado = Modulo(ReadIndex, Size) != Modulo(TotalWritten - 1, Size)
+          && 0 <= ReadIndex && ReadIndex <= TotalWritten;
+      Assert.AreEqual(_cont, _resultado);
     }
 
     /*[TestMethod()]
-    public void HasBackwardTest() {
-      Assert.Fail();
-    }
-
-    [TestMethod()]
-    public void BackwardReadTest() {
-      Assert.Fail();
-    }
-
-    [TestMethod()]
     public void HasForwardTest() {
       Assert.Fail();
     }
